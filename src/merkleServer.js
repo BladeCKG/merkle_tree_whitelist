792b2774095c37940a8893bf796e29f15884395f
Gurkaran Sahni <gurkaransahni@gmail.com>
2022-02-23 05:47:56 +0530

    add: server files

---
+++ src/merkleServer.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const detailsAPI = require('./merkleTree.js');
+
+const port = 8080;
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).send(`Hello, WORLD!`);
+  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: '*',
+
+        methods: [
+          'GET',
+          'POST',
+        ],
+      
+        allowedHeaders: [
+          'Content-Type',
+        ],      
+        origin: [],
+        credentials: true,
+        preflightContinue: false,
+    }),
+);
+
+app.set('trust proxy', 1);
+
+// app.use('/loginApis/', loginApi);
+app.use('/apis/merkleTree/', detailsAPI);
+
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`);
+});
--- src/merkleTree.js
+++ src/merkleTree.js
@@ -1,9 +1,14 @@
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
+const data = require("../data/data.csv")
 const fs = require('fs');
 const { parse } = require('csv-parse');
+const express = require('express');
+const router = express.Router();
 
-var whitelistedAddresses=[];
+let whitelistedAddresses=[];
+let merkleTree;
+
 fs.createReadStream("/Users/gurkaransahni/Projects/merkle_tree_whitelist/data/data.csv")
     .pipe(parse({delimiter: ':'}))
     .on('data', function(csvrow) {
@@ -16,15 +21,12 @@ fs.createReadStream("/Users/gurkaransahni/Projects/merkle_tree_whitelist/data/da
       const { merkleTree, rootHash } = await merkleTreeCalculation(whitelistedAddresses)
       await merkleTreeProofGeneration(merkleTree, "0x096DABeFE2DE1DeAF8E40368918d7B171aAf911c")
     });
-// let whitelistedAddresses = [
-//     "0x096DABeFE2DE1DeAF8E40368918d7B171aAf911c",
-//     "0x51Bb0896bFBa11e2277323A5f3a6f54C506bFFF9",
-//     "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
-//     "0x4d1a3EDEFD6F14b5dbdF3F7da21b9536e8760292",
-// ]
+
+console.log({data})
+
 const merkleTreeCalculation = (whitelistedAddresses) => {
     const leafNodes = whitelistedAddresses.map(address => keccak256(address));
-    const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+    merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 
     const rootHash = merkleTree.getRoot();
     console.log(merkleTree.toString())
@@ -43,4 +45,31 @@ const merkleTreeProofGeneration = (merkleTree, userAddress) => {
         claimingAddress: claimingAddress.toString('hex'),
         hexProof,
     })
+
+    return { hexProof }
 }
+
+router.get('/calculateMerkleTree', async (req, res) => {
+    console.log('THIS IS REQUEST QUERY', req.query);
+    const { userAddress } = req.query
+    try {
+        const {merkleTree, rootHash} = await merkleTreeCalculation(whitelistedAddresses)
+        res.status(200).json([{ data: rootHash }]);
+    } catch(err) {
+        res.status(404).json([{ err }]);
+    }
+});
+
+router.get('/getProof', async (req, res) => {
+    console.log('THIS IS REQUEST QUERY', req.query);
+    const { userAddress } = req.query
+    try {
+        const {hexProof} = await merkleTreeProofGeneration(merkleTree, userAddress)
+        res.status(200).json([{ data: hexProof }]);
+    } catch(err) {
+        console.log(err)
+        res.status(404).json([{ err }]);
+    }
+});
+
+module.exports = router;
